0d60cd5b0a8558f61e7ef790ff13a8d61ef5c36e
Can Baran Gündüz <canbaran011@gmail.com>
2023-12-03 12:51:01 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -11,14 +12,17 @@ export class AppComponent {
 
   courses = COURSES;
 
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
+
   // coreCourse = COURSES[0];
   // rxjsCourse = COURSES[1];
   // ngrxCourse = COURSES[2];
 
-  onCardClicked() {
-    console.log("card component clicked");
+  // onCardClicked() {
+  //   console.log("card component clicked");
     
-  }
+  // }
 
   onCourseSelected(course: Course) {
     console.log("On Course Selected !", course);
