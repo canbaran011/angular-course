8ad6b1217de46a6fd403a8fbb779f8a22eea4003
Can Baran Gündüz <canbaran011@gmail.com>
2023-12-10 19:26:24 +0300

    section 3 ends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
@@ -8,18 +8,46 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
   footerText: string = "FOOTER TEXT";
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
+  @ViewChild('cardRef1', {read: ElementRef}) 
+  card1: CourseCardComponent;
+
+  // @ViewChild('cardRef2') //CourseCardComponent
+  // card2: CourseCardComponent;
+
+  // @ViewChild('courseImage') //cant do this here NO
+  // courseImage: ElementRef;
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
+
+  @ViewChildren(CourseCardComponent, { read: ElementRef})
+  cards: QueryList<ElementRef>;
 
   // coreCourse = COURSES[0];
   // rxjsCourse = COURSES[1];
   // ngrxCourse = COURSES[2];
+
+  constructor() {
+    // console.log('containerDiv', this.containerDiv);
+    
+  }
+  ngAfterViewInit(): void {
+    // console.log("afterViewInit ",this.containerDiv);
+    // console.log("courseImage", this.courseImage);
+    // console.log("VChildren ", this.cards);
+    // this.courses[0].description = 'test'; //avoid modification in here
+    
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    )
+  
+  }
 
   // onCardClicked() {
   //   console.log("card component clicked");
@@ -27,10 +55,27 @@ export class AppComponent {
   // }
 
   onCourseSelected(course: Course) {
-    console.log("On Course Selected !", course);
+    // console.log("On Course Selected !", course);
+    console.log("card1", this.card1);
+    console.log("container", this.containerDiv);
+    // console.log("card2", this.card2);
     
   }
+
   onFooterSelected(some: any) {
     console.log(some)
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: "Angular Core Deep Dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+    }
+    )
   }
 }
